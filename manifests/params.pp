3b839c576a9e32066cd5ed96c8596e5b464d8956
Emilien Macchi <emilien.macchi@enovance.com>
2014-12-29 21:11:23 -0500

    Fix unit tests
    * fix reassignment in params.pp (it was a typo)
    * delete useless param in swift rspec

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@ class gnocchi::params {
   case $::osfamily {
     'RedHat': {
       $api_package_name         = 'openstack-gnocchi-api'
-      $api_package_name         = 'openstack-gnocchi-api'
+      $api_service_name         = 'openstack-gnocchi-api'
     }
     'Debian': {
       $api_package_name         = 'gnocchi-api'
--- spec/classes/gnocchi_storage_swift_spec.rb
+++ spec/classes/gnocchi_storage_swift_spec.rb
@@ -14,9 +14,8 @@ describe 'gnocchi::storage::swift' do
     context 'with default parameters' do
       it 'configures gnocchi-api with default parameters' do
         should contain_gnocchi_config('storage/driver').with_value('swift')
-        should contain_gnocchi_config('storage/swift_user').with_value('admin')
+        should contain_gnocchi_config('storage/swift_user').with_value('admin:admin')
         should contain_gnocchi_config('storage/swift_key').with_value('admin')
-        should contain_gnocchi_config('storage/swift_tenant_name').with_value('admin:admin')
         should contain_gnocchi_config('storage/swift_authurl').with_value('http://localhost:8080/auth/v1.0')
         should contain_gnocchi_config('storage/swift_auth_version').with_value('1')
       end
