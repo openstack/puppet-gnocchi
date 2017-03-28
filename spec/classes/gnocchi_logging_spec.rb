5dba91b9a87eb8ade28af444017d9d8d54f5d0d2
Mykyta Karpin <mkarpin@mirantis.com>
2017-03-28 17:28:50 +0300

    oslo log: check puppet resource instead of actual config in spec
    Change-Id: I99b729438aa208906117bdb3ab466b56cd1e6650

--- spec/classes/gnocchi_logging_spec.rb
+++ spec/classes/gnocchi_logging_spec.rb
@@ -16,25 +16,25 @@ describe 'gnocchi::logging' do
       :log_config_append => '/etc/gnocchi/logging.conf',
       :publish_errors => true,
       :default_log_levels => {
-        'amqp' => 'WARN', 'amqplib' => 'WARN', 'boto' => 'WARN',
-        'sqlalchemy' => 'WARN', 'suds' => 'INFO', 'iso8601' => 'WARN',
-        'requests.packages.urllib3.connectionpool' => 'WARN' },
-      :fatal_deprecations => true,
-      :instance_format => '[instance: %(uuid)s] ',
-      :instance_uuid_format => '[instance: %(uuid)s] ',
-      :log_date_format => '%Y-%m-%d %H:%M:%S',
-      :use_syslog => true,
-      :use_stderr => false,
-      :log_facility => 'LOG_FOO',
-      :log_dir => '/tmp/gnocchi',
-      :debug => true,
+          'amqp' => 'WARN', 'amqplib' => 'WARN', 'boto' => 'WARN',
+          'sqlalchemy' => 'WARN', 'suds' => 'INFO', 'iso8601' => 'WARN',
+          'requests.packages.urllib3.connectionpool' => 'WARN' },
+     :fatal_deprecations => true,
+     :instance_format => '[instance: %(uuid)s] ',
+     :instance_uuid_format => '[instance: %(uuid)s] ',
+     :log_date_format => '%Y-%m-%d %H:%M:%S',
+     :use_syslog => true,
+     :use_stderr => false,
+     :log_facility => 'LOG_FOO',
+     :log_dir => '/var/log',
+     :debug => true,
     }
   end
 
   shared_examples_for 'gnocchi-logging' do
 
     context 'with basic logging options and default settings' do
-      it_configures 'basic default logging settings'
+      it_configures  'basic default logging settings'
     end
 
     context 'with basic logging options and non-default settings' do
@@ -55,57 +55,47 @@ describe 'gnocchi::logging' do
 
   shared_examples 'basic default logging settings' do
     it 'configures gnocchi logging settings with default values' do
-      is_expected.to contain_gnocchi_config('DEFAULT/use_syslog').with(:value => '<SERVICE DEFAULT>')
-      is_expected.to contain_gnocchi_config('DEFAULT/use_stderr').with(:value => '<SERVICE DEFAULT>')
-      is_expected.to contain_gnocchi_config('DEFAULT/syslog_log_facility').with(:value => '<SERVICE DEFAULT>')
-      is_expected.to contain_gnocchi_config('DEFAULT/log_dir').with(:value => '/var/log/gnocchi')
-      is_expected.to contain_gnocchi_config('DEFAULT/debug').with(:value => '<SERVICE DEFAULT>')
+      is_expected.to contain_oslo__log('gnocchi_config').with(
+        :use_syslog          => '<SERVICE DEFAULT>',
+        :use_stderr          => '<SERVICE DEFAULT>',
+        :syslog_log_facility => '<SERVICE DEFAULT>',
+        :log_dir             => '/var/log/gnocchi',
+        :debug               => '<SERVICE DEFAULT>',
+      )
     end
   end
 
   shared_examples 'basic non-default logging settings' do
     it 'configures gnocchi logging settings with non-default values' do
-      is_expected.to contain_gnocchi_config('DEFAULT/use_syslog').with(:value => 'true')
-      is_expected.to contain_gnocchi_config('DEFAULT/use_stderr').with(:value => 'false')
-      is_expected.to contain_gnocchi_config('DEFAULT/syslog_log_facility').with(:value => 'LOG_FOO')
-      is_expected.to contain_gnocchi_config('DEFAULT/log_dir').with(:value => '/tmp/gnocchi')
-      is_expected.to contain_gnocchi_config('DEFAULT/debug').with(:value => 'true')
+      is_expected.to contain_oslo__log('gnocchi_config').with(
+        :use_syslog          => true,
+        :use_stderr          => false,
+        :syslog_log_facility => 'LOG_FOO',
+        :log_dir             => '/var/log',
+        :debug               => true,
+      )
     end
   end
 
   shared_examples_for 'logging params set' do
     it 'enables logging params' do
-      is_expected.to contain_gnocchi_config('DEFAULT/logging_context_format_string').with_value(
-        '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [%(request_id)s %(user_identity)s] %(instance)s%(message)s')
-
-      is_expected.to contain_gnocchi_config('DEFAULT/logging_default_format_string').with_value(
-        '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [-] %(instance)s%(message)s')
-
-      is_expected.to contain_gnocchi_config('DEFAULT/logging_debug_format_suffix').with_value(
-        '%(funcName)s %(pathname)s:%(lineno)d')
-
-      is_expected.to contain_gnocchi_config('DEFAULT/logging_exception_prefix').with_value(
-       '%(asctime)s.%(msecs)03d %(process)d TRACE %(name)s %(instance)s')
-
-      is_expected.to contain_gnocchi_config('DEFAULT/log_config_append').with_value(
-        '/etc/gnocchi/logging.conf')
-      is_expected.to contain_gnocchi_config('DEFAULT/publish_errors').with_value(
-        true)
-
-      is_expected.to contain_gnocchi_config('DEFAULT/default_log_levels').with_value(
-        'amqp=WARN,amqplib=WARN,boto=WARN,iso8601=WARN,requests.packages.urllib3.connectionpool=WARN,sqlalchemy=WARN,suds=INFO')
-
-      is_expected.to contain_gnocchi_config('DEFAULT/fatal_deprecations').with_value(
-        true)
-
-      is_expected.to contain_gnocchi_config('DEFAULT/instance_format').with_value(
-        '[instance: %(uuid)s] ')
-
-      is_expected.to contain_gnocchi_config('DEFAULT/instance_uuid_format').with_value(
-        '[instance: %(uuid)s] ')
-
-      is_expected.to contain_gnocchi_config('DEFAULT/log_date_format').with_value(
-        '%Y-%m-%d %H:%M:%S')
+      is_expected.to contain_oslo__log('gnocchi_config').with(
+        :logging_context_format_string =>
+          '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [%(request_id)s %(user_identity)s] %(instance)s%(message)s',
+        :logging_default_format_string => '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [-] %(instance)s%(message)s',
+        :logging_debug_format_suffix   => '%(funcName)s %(pathname)s:%(lineno)d',
+        :logging_exception_prefix      => '%(asctime)s.%(msecs)03d %(process)d TRACE %(name)s %(instance)s',
+        :log_config_append             => '/etc/gnocchi/logging.conf',
+        :publish_errors                => true,
+        :default_log_levels            => {
+          'amqp' => 'WARN', 'amqplib' => 'WARN', 'boto' => 'WARN',
+          'sqlalchemy' => 'WARN', 'suds' => 'INFO', 'iso8601' => 'WARN',
+          'requests.packages.urllib3.connectionpool' => 'WARN' },
+       :fatal_deprecations             => true,
+       :instance_format                => '[instance: %(uuid)s] ',
+       :instance_uuid_format           => '[instance: %(uuid)s] ',
+       :log_date_format                => '%Y-%m-%d %H:%M:%S',
+      )
     end
   end
 
@@ -116,7 +106,7 @@ describe 'gnocchi::logging' do
      :default_log_levels, :fatal_deprecations,
      :instance_format, :instance_uuid_format,
      :log_date_format, ].each { |param|
-        it { is_expected.to contain_gnocchi_config("DEFAULT/#{param}").with(:value => '<SERVICE DEFAULT>') }
+        it { is_expected.to contain_oslo__log('gnocchi_config').with("#{param}" => '<SERVICE DEFAULT>') }
       }
   end
 
