852a5b9f2f5672e520196344126fb6cbf7203624
ZhongShengping <chdzsp@163.com>
2017-05-23 14:59:43 +0800

    oslo policy: check puppet resource instead of actual config in spec
    Change-Id: Id5ceebd5d5d99db395218d9bbf2d514cb1199188

--- spec/classes/gnocchi_policy_spec.rb
+++ spec/classes/gnocchi_policy_spec.rb
@@ -20,7 +20,9 @@ describe 'gnocchi::policy' do
         :key   => 'context_is_admin',
         :value => 'foo:bar'
       })
-      is_expected.to contain_gnocchi_config('oslo_policy/policy_file').with_value('/etc/gnocchi/policy.json')
+      is_expected.to contain_oslo__policy('gnocchi_config').with(
+        :policy_file => '/etc/gnocchi/policy.json',
+      )
     end
   end
 
