50740e97a227a71488d3d0307d71d3d3ad4be671
Cyril Grosjean <cyril.grosjean@wifirst.fr>
2019-03-28 15:09:56 +0100

    Add gnocchi::storage::s3::s3_bucket_prefix into s3 storage configuration
    Change-Id: I9b0b61dc6d0b913b16876841baf2ed0b94f1fd77
    Closes-Bug: #1822111
    (cherry picked from commit 55e39f312618a2aa5d4e9dcfebf00e22d1e26b1e)

--- manifests/storage/s3.pp
+++ manifests/storage/s3.pp
@@ -21,12 +21,16 @@
 #   (optional) S3 storage secret access key.
 #   Defaults to undef
 #
+# [*s3_bucket_prefix*]
+#   (optional) S3 bucket prefix for gnocchi
+#   Defaults to undef
 #
 class gnocchi::storage::s3(
   $s3_endpoint_url      = $::os_service_default,
   $s3_region_name       = $::os_service_default,
   $s3_access_key_id     = undef,
   $s3_secret_access_key = undef,
+  $s3_bucket_prefix     = $::os_service_default,
 ) {
 
   include ::gnocchi::deps
@@ -37,6 +41,7 @@ class gnocchi::storage::s3(
     'storage/s3_region_name':        value => $s3_region_name;
     'storage/s3_access_key_id':      value => $s3_access_key_id;
     'storage/s3_secret_access_key':  value => $s3_secret_access_key;
+    'storage/s3_bucket_prefix':      value => $s3_bucket_prefix;
   }
 
 }
