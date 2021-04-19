0e1d398f56a251d0c77a6d6a133a9a10abcf3414
Takashi Kajinami <tkajinam@redhat.com>
2021-04-19 09:31:22 +0900

    Add support for oslo_policy/enforce_new_defaults
    Depends-on: https://review.opendev.org/781428
    Change-Id: I2cfa1267b08ea432a8bf3245ebb57abc5fadb809

--- spec/classes/gnocchi_policy_spec.rb
+++ spec/classes/gnocchi_policy_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe 'gnocchi::policy' do
-
-  shared_examples_for 'gnocchi::policy' do
+  shared_examples 'gnocchi::policy' do
     let :params do
       {
-        :enforce_scope => false,
-        :policy_path   => '/etc/gnocchi/policy.yaml',
-        :policy_dirs   => '/etc/gnocchi/policy.d',
-        :policies      => {
+        :enforce_scope        => false,
+        :enforce_new_defaults => false,
+        :policy_path          => '/etc/gnocchi/policy.yaml',
+        :policy_dirs          => '/etc/gnocchi/policy.d',
+        :policies             => {
           'context_is_admin' => {
             'key'   => 'context_is_admin',
             'value' => 'foo:bar'
@@ -26,22 +26,23 @@ describe 'gnocchi::policy' do
         :file_format => 'yaml',
       })
       is_expected.to contain_oslo__policy('gnocchi_config').with(
-        :enforce_scope => false,
-        :policy_file   => '/etc/gnocchi/policy.yaml',
-        :policy_dirs   => '/etc/gnocchi/policy.d',
+        :enforce_scope        => false,
+        :enforce_new_defaults => false,
+        :policy_file          => '/etc/gnocchi/policy.yaml',
+        :policy_dirs          => '/etc/gnocchi/policy.d',
       )
     end
   end
 
   on_supported_os({
-    :supported_os   => OSDefaults.get_supported_os
+    :supported_os => OSDefaults.get_supported_os
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_configures 'gnocchi::policy'
+      it_behaves_like 'gnocchi::policy'
     end
   end
 end
