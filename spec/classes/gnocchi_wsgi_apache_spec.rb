3be542d7b2bae588d1b4a97eefe57a5125a130f3
Takashi Kajinami <tkajinam@redhat.com>
2022-06-30 08:50:47 +0900

    Expose headers option of apache::vhost
    The headers option in apache::vhost is required in some case, for
    example when adding the X-XSS-Protection header. This change allows
    customizing the option for the api vhost.
    
    This change also adds support for request_headers so that both request
    headers and response headers can customized.
    
    Change-Id: I9fe841e9dc2a90627d0debdc9b6766a847390aed

--- spec/classes/gnocchi_wsgi_apache_spec.rb
+++ spec/classes/gnocchi_wsgi_apache_spec.rb
@@ -19,13 +19,15 @@ describe 'gnocchi::wsgi::apache' do
         :wsgi_script_dir             => platform_params[:wsgi_script_path],
         :wsgi_script_file            => 'app',
         :wsgi_script_source          => platform_params[:wsgi_script_source],
+        :headers                     => nil,
+        :request_headers             => nil,
         :custom_wsgi_process_options => {},
         :access_log_file             => false,
         :access_log_format           => false,
       )}
     end
 
-    context 'when overriding parameters using different ports' do
+    context 'when overriding parameters' do
       let :params do
         {
           :servername                => 'dummy.host',
@@ -41,6 +43,8 @@ describe 'gnocchi::wsgi::apache' do
           :access_log_file           => '/var/log/httpd/access_log',
           :access_log_format         => 'some format',
           :error_log_file            => '/var/log/httpd/error_log',
+          :headers                   => ['set X-XSS-Protection "1; mode=block"'],
+          :request_headers           => ['set Content-Type "application/json"'],
           :vhost_custom_fragment     => 'Timeout 99'
         }
       end
@@ -63,6 +67,8 @@ describe 'gnocchi::wsgi::apache' do
         :wsgi_script_dir           => platform_params[:wsgi_script_path],
         :wsgi_script_file          => 'app',
         :wsgi_script_source        => platform_params[:wsgi_script_source],
+        :headers                   => ['set X-XSS-Protection "1; mode=block"'],
+        :request_headers           => ['set Content-Type "application/json"'],
         :custom_wsgi_process_options => {
           'python_path' => '/my/python/path',
         },
