841e19d5a203d864ae92dc4c013f54004f2d4483
Tobias Urdin <tobias.urdin@binero.com>
2023-06-25 23:44:45 +0200

    Add per module policy service refresh
    Updating the policies for this project should only
    refresh the services that reads it.
    
    Change-Id: I5b7504bc02b2576a8921d1a70bc1bd07b19c1023
    (cherry picked from commit fea62d44e918fb5f944882ef01fa02e5687c7f31)

--- spec/classes/gnocchi_policy_spec.rb
+++ spec/classes/gnocchi_policy_spec.rb
@@ -33,6 +33,7 @@ describe 'gnocchi::policy' do
           :file_group   => 'gnocchi',
           :file_format  => 'yaml',
           :purge_config => false,
+          :tag          => 'gnocchi',
         )
         is_expected.to contain_oslo__policy('gnocchi_config').with(
           :enforce_scope        => false,
@@ -63,6 +64,7 @@ describe 'gnocchi::policy' do
           :file_group   => 'gnocchi',
           :file_format  => 'yaml',
           :purge_config => true,
+          :tag          => 'gnocchi',
         )
         is_expected.to contain_oslo__policy('gnocchi_config').with(
           :enforce_scope        => false,
